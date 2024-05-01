57c523150991e1271e430cbaaf0b0c63ca5c88eb
Azabu-bound <tylerjbailey95@gmail.com>
2024-05-01 16:35:51 -0400

    basic game loop

---
+++ src/main.py
@@ -0,0 +1,34 @@
+import pygame
+from pygame.locals import *
+from const import *
+
+class GameController(object):
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode(SCREENSIZE, 0, 32)
+        self.background = None
+
+    def set_background(self):
+        self.background = pygame.surface.Surface(SCREENSIZE).convert()
+        self.background.fill(BLACK)
+
+    def start_game(self):
+        self.set_background()
+
+    def update(self):
+        self.check_events()
+        self.render()
+
+    def check_events(self):
+        for event in pygame.event.get():
+            if event.type == QUIT: exit()
+
+    def render(self):
+        pygame.display.update()
+
+
+if __name__ == "__main__":
+    game = GameController()
+    game.start_game()
+    while True:
+        game.update()
