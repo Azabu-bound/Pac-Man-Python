f31c8958b6b81f0fa10dbe32f5beca5f57ae0dc2
Azabu-bound <tylerjbailey95@gmail.com>
2024-05-03 15:42:15 -0400

    Inky, Pinky, and Clyde added

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from const import *
 from pacman import Pacman
 from nodes import NodeGroup
 from pellets import Pellets_Group
-from spooky import GHOST, Ghost
+from spooky import GhostGroup
 
 class GameController(object):
     def __init__(self):
@@ -26,13 +26,16 @@ class GameController(object):
         self.nodes.connect_home_nodes(homekey, (15, 14), RIGHT)
         self.pacman = Pacman(self.nodes.get_start_temp_node())
         self.pellets = Pellets_Group("maze1.txt")
-        self.ghost = Ghost(self.nodes.get_start_temp_node(), self.pacman)
-        self.ghost.set_spawn_node(self.nodes.get_node_from_tiles(2 + 11.5, 3 + 14))
+        #self.ghost = Ghost(self.nodes.get_start_temp_node(), self.pacman)
+        self.ghosts = GhostGroup(self.nodes.get_start_temp_node(), self.pacman)
+        #self.ghost.set_spawn_node(self.nodes.get_node_from_tiles(2 + 11.5, 3 + 14))
+        self.ghosts.set_spawn_node(self.nodes.get_node_from_tiles(2 + 11.5, 3 + 14))
 
     def update(self):
         dt = self.clock.tick(30) / 1000.0
         self.pacman.update(dt)
-        self.ghost.update(dt)
+        #self.ghost.update(dt)
+        self.ghosts.update(dt)
         self.pellets.update(dt)
         self.pellet_events()
         self.check_ghost_events()
@@ -40,9 +43,13 @@ class GameController(object):
         self.render()
 
     def check_ghost_events(self):
-        if self.pacman.collide_ghost(self.ghost):
-            if self.ghost.mode.current is FREIGHT:
-                self.ghost.start_spawn()
+        for ghost in self.ghosts:
+            if self.pacman.collide_ghost(ghost):
+                if ghost.mode.current is FREIGHT:
+                    ghost.start_spawn()
+        #if self.pacman.collide_ghost(self.ghost):
+            #if self.ghost.mode.current is FREIGHT:
+                #self.ghost.start_spawn()
 
     def check_events(self):
         for event in pygame.event.get():
@@ -53,7 +60,8 @@ class GameController(object):
         self.nodes.render(self.screen)
         self.pellets.render(self.screen)
         self.pacman.render(self.screen)
-        self.ghost.render(self.screen)
+        #self.ghost.render(self.screen)
+        self.ghosts.render(self.screen)
         pygame.display.update()
 
     def pellet_events(self):
@@ -62,7 +70,8 @@ class GameController(object):
             self.pellets.number_eaten += 1
             self.pellets.pellet_list.remove(p)
             if p.name == POWERUPPELLET:
-                self.ghost.start_freight()
+                #self.ghost.start_freight()
+                self.ghosts.start_freight()
 
 if __name__ == "__main__":
     game = GameController()
--- src/spooky.py
+++ src/spooky.py
@@ -10,7 +10,7 @@ from actions import ContollerOfModes
 from pacman import Pacman
 
 class Ghost(Entity):
-    def __init__(self, node, pacman=None):
+    def __init__(self, node, pacman=None, blinky=None):
         Entity.__init__(self, node)
         self.name = GHOST
         self.points = 200
@@ -18,6 +18,8 @@ class Ghost(Entity):
         self.direction_method = self.astar_direction
         self.pacman = pacman
         self.mode = ContollerOfModes(self)
+        self.blinky = blinky
+        self.home_node = node
 
     def update(self, dt) -> None:
         self.mode.update(dt)
@@ -61,3 +63,121 @@ class Ghost(Entity):
             self.set_speed(150)
             self.direction_method = self.astar_direction
             self.spawn()
+
+class Blinky(Ghost):
+    def __init__(self, node, pacman=None, blinky=None):
+        Ghost.__init__(self, node, pacman, blinky)
+        self.name = BLINKY
+        self.color = RED
+
+class Pinky(Ghost):
+    def __init__(self, node, pacman=None, blinky=None):
+        Ghost.__init__(self, node, pacman, blinky)
+        self.name = PINKY
+        self.color = PINK
+
+    def scatter(self):
+        self.goal = Vector2(TITLEWIDTH * NCOLS, 0)
+
+    def chase(self):
+        pacman_direction = self.pacman.direction
+        pacman_position = self.pacman.position
+        target_position = pacman_position + self.pacman.directions[pacman_direction] * TITLEWIDTH * 4
+        self.goal = target_position
+
+class Inky(Ghost):
+    def __init__(self, node, pacman=None, blinky=None):
+        Ghost.__init__(self, node, pacman, blinky)
+        self.name = INKY
+        self.color = TEAL
+        self.blinky = blinky
+
+    def scatter(self):
+        self.goal = Vector2(TITLEWIDTH * NCOLS, TITLEHEIGHT * NROWS)
+
+    def chase(self):
+        pacman_direction = self.pacman.direction
+        pacman_position = self.pacman.position
+        blinky_position = self.blinky.position
+
+        # Calculate the position 2 tiles in front of Pac-Man
+        vec1 = pacman_position + self.pacman.directions[pacman_direction] * TITLEWIDTH * 2
+
+        # Calculate the vector from Blinky to the position 2 tiles in front of Pac-Man
+        vec2 = (vec1 - blinky_position) * 2
+
+        # Set Inky's goal to the position obtained by adding vec2 to Blinky's position
+        self.goal = blinky_position + vec2
+
+class Clyde(Ghost):
+    def __init__(self, node, pacman=None, blinky=None):
+        Ghost.__init__(self, node, pacman, blinky)
+        self.name = CLYDE
+        self.color = ORANGE
+
+    def scatter(self):
+        self.goal = Vector2(0, TITLEHEIGHT * NROWS)
+
+    def chase(self):
+        d = self.pacman.position - self.position
+        d_square = d.magnitude_squared()
+        if d_square <= (TITLEWIDTH * 8) ** 2:
+            self.scatter()
+        else:
+            pacman_direction = self.pacman.direction
+            pacman_position = self.pacman.position
+            self.goal = pacman_position + self.pacman.directions[pacman_direction] * TITLEWIDTH * 4
+
+
+class GhostGroup(object):
+    def __init__(self, node, pacman):
+        self.blinky = Blinky(node, pacman)
+        self.pinky = Pinky(node, pacman)
+        self.inky = Inky(node, pacman, self.blinky)
+        self.clyde = Clyde(node, pacman)
+        self.ghosts = [
+            self.blinky,
+            self.pinky,
+            self.inky,
+            self.clyde
+        ]
+
+    def __iter__(self):
+        return iter(self.ghosts)
+
+    def update(self, dt):
+        for ghost in self:
+            ghost.update(dt)
+
+    def start_freight(self):
+        for ghost in self:
+            ghost.start_freight()
+        self.reset_points()
+
+    def set_spawn_node(self, node):
+        for ghost in self:
+            ghost.set_spawn_node(node)
+
+    def update_points(self):
+        for ghost in self:
+            ghost.points *= 2
+
+    def reset_points(self):
+        for ghost in self:
+            ghost.points = 200
+
+    def reset(self):
+        for ghost in self:
+            ghost.reset()
+
+    def hide(self):
+        for ghost in self:
+            ghost.visible = False
+
+    def show(self):
+        for ghost in self:
+            ghost.visible = True
+
+    def render(self, screen):
+        for ghost in self:
+            ghost.render(screen)
